eaaa3aae38ff500f04a65ed75f937d8175862376
55408suga <55408suga.ka@gmail.com>
2025-09-11 10:15:17 +0000

    Feat: Add details for all gyms

--- server.js
+++ server.js
@@ -30,6 +30,60 @@ const gyms = [
         price: '1日 2,200円(男性)/1,760円(女性) (学生料金見当たらず)',
         googleMapEmbed: 'https://www.google.com/maps/embed/v1/place?q=神奈川県横浜市中区山下町22',
         googleRating: '4.1 (160件のレビュー)'
+    },
+    {
+        id: 4,
+        name: 'ボルダリングジムシェア',
+        description: '京急線「鶴見市場駅」から徒歩6分。神奈川最大級の広さ。',
+        website: 'https://www.boulderinggymshare.com/',
+        price: '学生料金 1,650円',
+        googleMapEmbed: 'https://www.google.com/maps/embed/v1/place?q=ボルダリングジムシェア 鶴見',
+        googleRating: '4.4 (90件のレビュー)'
+    },
+    {
+        id: 5,
+        name: 'B-PUMP横浜店',
+        description: '横浜駅から徒歩10分。予約不要で体験クライミングが可能。',
+        website: 'https://pump-climbing.com/gym/bpump-yokohama/',
+        price: '学生料金 1,650円 (平日)',
+        googleMapEmbed: 'https://www.google.com/maps/embed/v1/place?q=B-PUMP横浜店',
+        googleRating: '4.1 (159件のレビュー)'
+    },
+    {
+        id: 6,
+        name: 'クライミングジム プロジェクト',
+        description: '横浜線「鴨居駅」から徒歩16分。',
+        website: 'https://project-climbing.com/',
+        price: '学生料金 1,700円',
+        googleMapEmbed: 'https://www.google.com/maps/embed/v1/place?q=クライミングジム プロジェクト 鴨居 神奈川県横浜市都筑区佐江戸町417',
+        googleRating: '4.5 (47件のレビュー)'
+    },
+    {
+        id: 7,
+        name: 'all fours GYM (現: Rock Spot)',
+        description: '世界初の「四足教室専用レーン」があるジム。',
+        website: 'http://allfoursgym.com/',
+        price: '学生料金 1,400円 (大学生)',
+        googleMapEmbed: 'https://maps.google.com/maps?q=神奈川県横浜市中区本牧原15-6 グロブナースクエア地下1階&output=embed',
+        googleRating: '評価なし'
+    },
+    {
+        id: 8,
+        name: 'folk bouldering gym',
+        description: '室内でボルダリングを楽しめるクライミングジム。',
+        website: 'https://folkboulderinggym.com/',
+        price: '学生料金 1,700円',
+        googleMapEmbed: 'https://www.google.com/maps/embed/v1/place?q=folk+bouldering+gym+横浜市港北区新羽町576-1',
+        googleRating: '4.3 (29件のレビュー)'
+    },
+    {
+        id: 9,
+        name: 'クライミングバム横浜店',
+        description: '高さが怖い方でも楽しめるジム。',
+        website: 'https://climbingbum.jp/yokohama/',
+        price: '1日 1,800円 (学生料金見当たらず)',
+        googleMapEmbed: 'https://www.google.com/maps/embed/v1/place?q=クライミングバム横浜店',
+        googleRating: '4.0 (46件のレビュー)'
     }
 ];
 
